bdc610d3694920aae07ee9abca55da15e6e26cd6
Gabriel Fortuna <gabriel@zero-one.io>
2022-03-08 12:58:21 +0200

    style: clean up code readability

--- lib/sa_vat_validation.rb
+++ lib/sa_vat_validation.rb
@@ -7,30 +7,31 @@ module SaVatValidation
 
     # As per SARS
     # A VAT Number is a unique number, which comprises of 10 digits and starts with the number 4
-    return false unless digits.first.to_i == 4
-    return false unless digits.size == 10 && vat_number.to_s.is_numeric?
+    return false unless digits.first == 4
+    return false unless digits.size  == 10 && vat_number.to_s.is_numeric?
 
     check_digit = digits.pop
+    sum         = 0
 
-    sum = 0
     digits.each_with_index do |digit, i|
       if i.even?
         result = digit * 2
-        if result >= 10
-          result = first_digit(result) + last_digit(result)
-        end
+        result = first_digit(result) + last_digit(result) if result >= 10
+
         sum += result
       else
         sum += digit.to_i
       end
     end
+
     if (last_digit(sum)).zero?
       calculated_digit = last_digit(sum)
     else
       calculated_digit = 10 - last_digit(sum)
     end
 
-    return check_digit == calculated_digit
+    check_digit == calculated_digit
+  end
 
   def self.last_digit(a)
     a.to_s.chars.last.to_i
@@ -39,6 +40,8 @@ module SaVatValidation
   def self.first_digit(a)
     a.to_s.chars.first.to_i
   end
+end
+
 class String
   def valid_sa_vat_number?
     SaVatValidation::valid?(self)
